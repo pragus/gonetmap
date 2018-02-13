2afc55766b42b4ca089ca438b9c04a6be217b980
Roman Timofeev <cryptoloop@gmail.com>
2018-02-13 19:06:05 +0300

    use pure go

--- main.go
+++ main.go
@@ -5,14 +5,17 @@ import (
 	"log"
 	"os"
 	"gonetmap"
+
 )
 
-func ProcessRing(r *gonetmap.NmRing) (uint16) {
+
+
+func ProcessRing(r *gonetmap.NetmapRing) (uint16) {
 	var i uint16
 	cur := r.Cur
 	for i := 0; !r.RingIsEmpty(); i++ {
-		slot_ptr := r.Slot(cur)
-		buf_ptr := r.SlotBuffer(slot_ptr)
+		SlotPtr := r.Slot(cur)
+		buf_ptr := r.SlotBuffer(SlotPtr)
 		_ = buf_ptr
 		cur = r.Next(cur)
 	}
@@ -21,25 +24,40 @@ func ProcessRing(r *gonetmap.NmRing) (uint16) {
 
 }
 
+
 func main() {
-	var (
-		iface = flag.String("i", "", "interface")
-	)
+	iface := flag.String("i", "", "interface")
 	flag.Parse()
+
 	if *iface == "" {
 		log.Println("usage nm -i netmap:p{0")
 		os.Exit(1)
 	}
-	nm := gonetmap.New()
-	if err := nm.COpen(*iface); err != nil {
-		log.Println(err)
-		return
-	}
 
-	ring := nm.OpenRing(uint32(nm.Desc.LastRxRing), false)
+	nm := gonetmap.New()
+
+
+
+	r0 := gonetmap.Request{Version:11, RingId:0, Flags:gonetmap.PipeMaster}
+	r0.Arg1 = 1
+	r0.SetName("p")
+
+	n0, _ := nm.RegIf(&r0)
+	rx0 := n0.OpenRing(0, gonetmap.RX)
+	//fmt.Printf("%+v\n\n", *rx0)
+
+
+	//os.Exit(1)
+	//fmt.Printf("name:%+v %+v\n\n", s, *ring)
+	//os.Exit(0)
+	//ring := nm.OpenRing(nm.Descriptor.LastRxRing, gonetmap.RX)
+	//fmt.Printf("%p=%+v\n\n", &nm.Descriptor, nm.Descriptor)
+	//ring := nm.OpenRing(nm.Descriptor.LastRxRing, gonetmap.RX)
+
 	for {
 		nm.Poll(-1)
-		ProcessRing(ring)
+		ProcessRing(rx0)
+
 	}
-	defer nm.Close()
+
 }
