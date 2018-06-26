dedf9dfff9439849d8e6a85df6aad81a697f6eac
pragus <cryptoloop@gmail.com>
2018-06-27 00:57:45 +0300

    simplify piper example

--- examples/piperx/main.go
+++ examples/piperx/main.go
@@ -6,15 +6,15 @@ import (
 )
 
 type EtherHdr struct {
-	DAddr     [6]uint
+	DAddr     [6]uint8
 	SAddr     [6]uint8
-	EtherType uint16
+	EtherType [2]uint8
 }
 
 func ProcessSlot(r *gonetmap.NetmapRing, s *gonetmap.Slot) {
 	buf := r.SlotBuffer(s)
 	eth := (*EtherHdr)(buf)
-	eth.EtherType = 0x800
+	eth.EtherType = [2]byte{0x81, 00}
 }
 
 func ProcessRing(r *gonetmap.NetmapRing) uint16 {
@@ -32,7 +32,7 @@ func ProcessRing(r *gonetmap.NetmapRing) uint16 {
 
 func PollingWorker(nif *gonetmap.Interface, ring *gonetmap.NetmapRing, timeout int) {
 	fd := int32(nif.File.Fd())
-	events := make([]unix.PollFd, 1, 1)
+	events := make([]unix.PollFd, 1)
 	events[0] = unix.PollFd{Fd: fd, Events: unix.POLLIN, Revents: 0}
 	for {
 		unix.Poll(events, timeout)
@@ -46,15 +46,8 @@ func main() {
 	req0 := gonetmap.Request{Version: 11, RingId: 0, Flags: gonetmap.ReqPipeMaster, Arg1: 0}
 	req0.SetName("p")
 
-	req1 := gonetmap.Request{Version: 11, RingId: 1, Flags: gonetmap.ReqPipeMaster, Arg1: 0}
-	req1.SetName("p")
-
 	iface0, _ := netmap.RegIf(&req0)
-	iface1, _ := netmap.RegIf(&req1)
 	rxq0 := iface0.OpenRing(0, gonetmap.RX)
-	rxq1 := iface1.OpenRing(0, gonetmap.RX)
-
-	go PollingWorker(iface0, rxq0, 0)
-	PollingWorker(iface1, rxq1, 0)
+	PollingWorker(iface0, rxq0, 0)
 
 }
