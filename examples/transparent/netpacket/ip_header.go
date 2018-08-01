8c2be04a192ac205eeb3e9544831e321573d337a
Roman Timofeev <cryptoloop@gmail.com>
2018-08-01 19:33:10 +0300

    faster ipv4 csum calculation

--- examples/transparent/netpacket/ip_header.go
+++ examples/transparent/netpacket/ip_header.go
@@ -4,7 +4,22 @@ import (
 	"encoding/binary"
 	"unsafe"
 	"github.com/pragus/gonetmap"
+
 )
+
+const (
+	Idx0 = 0
+	Idx1 = 2
+	Idx2 = 4
+	Idx3 = 6
+	Idx4 = 8
+	Idx5 = 12
+	Idx6 = 14
+	Idx7 = 16
+	Idx8 = 18
+
+)
+
 
 type IPv4Hdr struct {
 	VersionIhl     uint8             // version and header length
@@ -20,21 +35,23 @@ type IPv4Hdr struct {
 }
 
 func (h *IPv4Hdr) UpdateChecksum() {
-	var chk uint32
-	b := *(*[]byte)(gonetmap.PtrSliceFrom(unsafe.Pointer(h), IPv4MinLen))
-	for i := 0; i < IPv4MinLen; i += 2 {
-		// Iterating two bytes at a time; checksum bytes occur at offsets
-		// 10 and 11.  Skip them.
-		if i == 10 {
-			continue
-		}
 
-		chk += uint32(binary.BigEndian.Uint16(b[i: i+2]))
-	}
+	b := *(*[]byte)(gonetmap.PtrSliceFrom(unsafe.Pointer(h), IPv4MinLen))
+
+	p0 := uint32(binary.BigEndian.Uint16(b[Idx0: Idx0+2]))
+	p1 := uint32(binary.BigEndian.Uint16(b[Idx1: Idx1+2]))
+	p2 := uint32(binary.BigEndian.Uint16(b[Idx2: Idx2+2]))
+	p3 := uint32(binary.BigEndian.Uint16(b[Idx3: Idx3+2]))
+	p4 := uint32(binary.BigEndian.Uint16(b[Idx4: Idx4+2]))
+	p5 := uint32(binary.BigEndian.Uint16(b[Idx5: Idx5+2]))
+	p6 := uint32(binary.BigEndian.Uint16(b[Idx6: Idx6+2]))
+	p7 := uint32(binary.BigEndian.Uint16(b[Idx7: Idx7+2]))
+	p8 := uint32(binary.BigEndian.Uint16(b[Idx8: Idx8+2]))
+	chk := p0 + p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8
 
 	// "The first 4 bits are the carry and will be added to the rest of
 	// the value."
 	carry := uint16(chk >> 16)
-	csum := ^(carry + uint16(chk&0x0ffff))
+	csum := ^(carry + uint16(chk & 0x0ffff))
 	h.HdrChecksum = Htons(csum)
 }
